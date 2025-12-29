45456fefcde3ccf320eaefe0f1dd666c26b084c4
Perikiss <qilimbayp@gmail.com>
2025-12-29 21:53:35 +0500

    Implement SocialItem class with author and methods

--- Social Media Networking platform/src/SocialItem.java
+++ Social Media Networking platform/src/SocialItem.java
@@ -1,1 +1,29 @@
+import java.util.Objects;
 
+public abstract class SocialItem {
+    protected Profile author;
+
+    public SocialItem(Profile author) {
+        this.author = author;
+    }
+
+    public Profile getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SocialItem)) return false;
+        SocialItem that = (SocialItem) o;
+        return Objects.equals(author, that.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author);
+    }
+
+    @Override
+    public abstract String toString();
+}
