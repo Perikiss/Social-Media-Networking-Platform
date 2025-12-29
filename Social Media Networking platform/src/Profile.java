5fedcb7522b0654275acefea36a9d6058d1a360b
Perikiss <qilimbayp@gmail.com>
2025-12-29 21:51:08 +0500

    Refactor Profile class methods and variables

--- Social Media Networking platform/src/Profile.java
+++ Social Media Networking platform/src/Profile.java
@@ -1,45 +1,43 @@
+import java.util.Objects;
+
 public class Profile {
-    private String username;//открывайм variables
+    private String username;
     private String fullName;
     private int followers;
 
     public Profile(String username, String fullName, int followers) {
         this.username = username;
         this.fullName = fullName;
-        this.followers = followers;//меншіктеу
+        this.followers = followers;
     }
 
     public String getUsername() {
-        return username;//возвращаем юз
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void follow(Profile other){
-        other.setFollowers(other.getFollowers() +1);
-        System.out.println(this.username +" " + "followed" + " " + other.username);
+        return username;
     }
 
     public int getFollowers() {
         return followers;
     }
 
-    public void setFollowers(int followers) {
-        this.followers = followers;
+    public void follow(Profile other) {
+        other.followers++;
     }
 
-    public void display() {
-        System.out.println("Profile: " + " "+ fullName +" " +" (@" + username + "),  Followers: " +" " +followers);
+    @Override
+    public String toString() {
+        return fullName + " (@" + username + "), followers: " + followers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Profile)) return false;
+        Profile profile = (Profile) o;
+        return Objects.equals(username, profile.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
     }
 }
-
